fa6363c3fcdeb52f4887fefe7e49934712ced3b3
Michael Brunner <michaelbrunn3r@gmail.com>
2023-12-04 15:18:47 +0100

    Solution 4-1

--- 2023/day-4/src/main.rs
+++ 2023/day-4/src/main.rs
@@ -8,11 +8,62 @@ fn main() {
 }
 
 fn task_0(input: &str) -> usize {
-    0
+    input
+        .lines()
+        .map(|line| Card::from_str(line))
+        // .inspect(|card| println!("{:?}", card))
+        .map(|card| card.count_matching())
+        // .inspect(|matches| println!("#matches={}", matches))
+        .filter(|matches| *matches > 0)
+        .map(|matches| 2usize.pow((matches - 1) as u32))
+        // .inspect(|matches| println!("#points={}", matches))
+        .sum::<usize>()
 }
 
 fn task_1(input: &str) -> usize {
     0
+}
+
+#[derive(Debug)]
+struct Card {
+    winning_numbers: Vec<usize>,
+    your_numbers: Vec<usize>,
+}
+
+impl Card {
+    fn from_str(input: &str) -> Self {
+        let (_, body) = input.split_once(':').unwrap();
+        let (winning_str, your_str) = body.split_once('|').unwrap();
+
+        let winning_numbers = winning_str
+            .trim()
+            .split(' ')
+            .filter(|s| !s.is_empty())
+            .map(|s| s.parse::<usize>().unwrap())
+            .collect::<Vec<_>>();
+
+        let your_numbers = your_str
+            .trim()
+            .split(' ')
+            .filter(|s| !s.is_empty())
+            .map(|s| s.parse::<usize>().unwrap())
+            .collect::<Vec<_>>();
+
+        Self {
+            winning_numbers,
+            your_numbers,
+        }
+    }
+
+    fn count_matching(&self) -> usize {
+        let mut matches = 0;
+        for number in self.your_numbers.iter() {
+            if self.winning_numbers.contains(number) {
+                matches += 1;
+            }
+        }
+        matches
+    }
 }
 
 #[cfg(test)]
