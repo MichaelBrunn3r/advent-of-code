cd0ff9314d67e24e21f98847809b1d1cf55234a5
Michael Brunner <michaelbrunn3r@gmail.com>
2023-12-06 10:31:36 +0100

    Add RangeToRangeMap

--- 2023/day-5/src/main.rs
+++ 2023/day-5/src/main.rs
@@ -1,6 +1,6 @@
-use aoc;
+use aoc::prelude::*;
 use itertools::Itertools;
-use regex::Regex;
+use std::ops::RangeInclusive;
 
 fn main() {
     let input = aoc::read_input_to_string();
@@ -9,59 +9,34 @@ fn main() {
 }
 
 fn task_0(input: &str) -> usize {
-    let mut maps = input.split("\n\n");
+    let mut section = input.split("\n\n");
 
-    let seeds: Vec<usize> = maps
+    let seeds: Vec<usize> = section
         .next()
         .unwrap()
         .split_once(": ")
         .unwrap()
         .1
-        .split(' ')
-        .map(|s| s.parse().unwrap())
+        .parse_splits(" ")
         .collect();
 
     // println!("{:?}", seeds);
 
-    let mappings = maps
-        .map(|s| s.lines())
-        .map(|mut lines| {
-            let (from, to) = lines
-                .next()
-                .unwrap()
-                .split_once(' ')
-                .unwrap()
-                .0
-                .split_once("-to-")
-                .unwrap();
-            (from, to, lines)
-        })
-        // .inspect(|(from, to, _)| println!("{} -> {}", from, to))
-        .map(|(from, to, lines)| {
-            let mappings: Vec<(usize, usize, usize)> = lines
-                .map(|line| {
-                    line.split(' ')
-                        .map(|s| s.parse::<usize>().unwrap())
-                        .next_tuple()
-                        .unwrap()
-                })
-                .collect();
-            (from, to, mappings)
-        })
-        // .inspect(|(_, _, mappings)| println!("{:?}", mappings))
+    let maps = section
+        .map(|s| parse_map_section(s.lines()))
         .collect::<Vec<_>>();
 
     let mut locations = seeds.clone();
 
-    for locations in locations.iter_mut() {
-        for (_, _, mappings) in mappings.iter() {
-            let default = (*locations, *locations, 1);
+    for location in locations.iter_mut() {
+        for mappings in maps.iter() {
+            let default = RangeToRangeMap::identity(*location);
             let mapping = mappings
                 .iter()
-                .find(|(_, source, len)| *locations >= *source && *locations <= *source + len)
+                .find(|map| map.from.contains(location))
                 .unwrap_or(&default);
 
-            *locations = mapping.0 + (*locations - mapping.1)
+            *location = mapping.map(*location);
         }
     }
 
@@ -74,67 +49,41 @@ fn task_0(input: &str) -> usize {
 }
 
 fn task_1(input: &str) -> usize {
-    let mut maps = input.split("\n\n");
+    let mut sections = input.split("\n\n");
 
-    let seed_ranges: Vec<(usize, usize)> = maps
+    let seed_ranges: Vec<RangeInclusive<usize>> = sections
         .next()
         .unwrap()
         .split_once(": ")
         .unwrap()
         .1
-        .split(' ')
-        .map(|s| s.parse().unwrap())
+        .parse_splits(" ")
         .chunks(2)
         .into_iter()
-        .map(|mut c| c.next_tuple().unwrap())
+        .map(|mut c| {
+            let (start, len) = c.next_tuple().unwrap();
+            start..=start + len
+        })
         .collect();
 
-    let mappings = maps
-        .map(|s| s.lines())
-        .map(|mut lines| {
-            let (from, to) = lines
-                .next()
-                .unwrap()
-                .split_once(' ')
-                .unwrap()
-                .0
-                .split_once("-to-")
-                .unwrap();
-            (from, to, lines)
-        })
-        // .inspect(|(from, to, _)| println!("{} -> {}", from, to))
-        .map(|(from, to, lines)| {
-            let mappings: Vec<(usize, usize, usize)> = lines
-                .map(|line| {
-                    line.split(' ')
-                        .map(|s| s.parse::<usize>().unwrap())
-                        .next_tuple()
-                        .unwrap()
-                })
-                .collect();
-            (from, to, mappings)
-        })
-        // .inspect(|(_, _, mappings)| println!("{:?}", mappings))
+    let maps = sections
+        .map(|s| parse_map_section(s.lines()))
         .collect::<Vec<_>>();
 
     let mut min = usize::MAX;
     for seed_range in seed_ranges.iter() {
         println!("Seed range={:?}", seed_range);
-        for (i, seed) in ((seed_range.0)..=(seed_range.0 + seed_range.1)).enumerate() {
+        for seed in seed_range.clone().progress(seed_range.len().into()) {
             // println!("Seed={}", seed);
-            if i % 5_000 == 0 {
-                let progress = i as f64 / seed_range.1 as f64;
-                print!("\rProgress: {:.2}%, Min={}", progress * 100.0, min);
-            }
             let mut current = seed;
-            for (_, _, mappings) in mappings.iter() {
-                let default = (current, current, 1);
+            for mappings in maps.iter() {
+                let default = RangeToRangeMap::identity(current);
                 let mapping = mappings
                     .iter()
-                    .find(|(_, source, len)| current >= *source && current <= *source + len)
+                    .find(|map| map.from.contains(&current))
                     .unwrap_or(&default);
 
-                current = mapping.0 + (current - mapping.1);
+                current = mapping.map(current);
                 // println!("{:?}->{}", mapping, current);
             }
 
@@ -145,6 +94,47 @@ fn task_1(input: &str) -> usize {
     }
 
     min
+}
+
+fn parse_map_section<'a>(lines: impl Iterator<Item = &'a str>) -> Vec<RangeToRangeMap> {
+    lines
+        .skip(1)
+        .map(|line| {
+            let (to_start, from_start, len) = line
+                .parse_splits::<usize>(" ")
+                .next_tuple::<(usize, usize, usize)>()
+                .unwrap();
+            RangeToRangeMap {
+                from: (from_start)..=(from_start + len),
+                to: (to_start)..=(to_start + len),
+            }
+        })
+        .collect()
+}
+
+#[derive(Debug)]
+struct RangeToRangeMap {
+    from: RangeInclusive<usize>,
+    to: RangeInclusive<usize>,
+}
+
+impl From<(RangeInclusive<usize>, RangeInclusive<usize>)> for RangeToRangeMap {
+    fn from((from, to): (RangeInclusive<usize>, RangeInclusive<usize>)) -> Self {
+        Self { from, to }
+    }
+}
+
+impl RangeToRangeMap {
+    fn identity(val: usize) -> Self {
+        Self {
+            from: val..=val,
+            to: val..=val,
+        }
+    }
+
+    fn map(&self, val: usize) -> usize {
+        self.to.start() + (val - self.from.start())
+    }
 }
 
 #[cfg(test)]
