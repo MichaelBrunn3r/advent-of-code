4e17c23ffb6df63eea31d6bc5777669057f129ec
Michael Brunner <michaelbrunn3r@gmail.com>
2023-12-08 18:01:43 +0100

    ref: Split off lut module

--- 2023/day-7/src/lib.rs
+++ 2023/day-7/src/lib.rs
@@ -1,18 +1,11 @@
+pub mod lut;
+
 use itertools::Itertools;
+use lut::*;
 
-const CARD_LUT_LEN: usize = 36;
-const SMALLEST_LABEL: usize = b'2' as usize;
+pub const CARD_LUT_LEN: usize = 36;
+pub const SMALLEST_LABEL: usize = b'2' as usize;
 const NUM_LABELS: usize = 13;
-const LABEL_TO_STRENGTH_LUT_0: [u32; CARD_LUT_LEN] =
-    generate_label_to_strength_lut(b"23456789TJQKA");
-const LABEL_TO_STRENGTH_LUT_1: [u32; CARD_LUT_LEN] =
-    generate_label_to_strength_lut(b"J23456789TQKA");
-
-const OCCURENCES_LUT_LEN: usize = (16 << 3) + 5 + 1;
-const COUNT_OCCURENCES_TO_HAND_KIND_LUT_0: [HandKind; OCCURENCES_LUT_LEN] =
-    generate_count_occurences_to_hand_kind_lut(false);
-const COUNT_OCCURENCES_TO_HAND_KIND_LUT_1: [HandKind; OCCURENCES_LUT_LEN] =
-    generate_count_occurences_to_hand_kind_lut(true);
 
 pub fn task_0(input: &str) -> usize {
     count_winnings(
@@ -95,7 +88,7 @@ impl HandKind {
     ) -> Self {
         let mut card_counts = 0u64;
         let mut count_occurences = NUM_LABELS as usize;
-        let mut joker_count = 0usize;
+        let mut num_jokers = 0usize;
 
         for l in labels.chars() {
             let card = card_lut[l as usize - SMALLEST_LABEL] as usize;
@@ -108,117 +101,11 @@ impl HandKind {
             let card_count = (card_counts >> 4 * card) & 0xf;
             count_occurences += 1usize << card_count * 4;
 
-            joker_count += (l == 'J') as usize;
+            num_jokers += (l == 'J') as usize;
         }
 
-        count_occurences_to_hand_kind(count_occurences, joker_count, occurences_lut)
+        occurences_lut[count_occurences_to_lut_idx(count_occurences) + num_jokers]
     }
-}
-
-const fn generate_label_to_strength_lut(labels: &[u8]) -> [u32; CARD_LUT_LEN] {
-    let mut map = [u32::MAX; CARD_LUT_LEN];
-
-    let mut i = 0;
-    while i < labels.len() {
-        map[labels[i] as usize - SMALLEST_LABEL] = i as u32;
-        i += 1;
-    }
-
-    map
-}
-
-const fn generate_count_occurences_to_hand_kind_lut(
-    joker_is_wildcard: bool,
-) -> [HandKind; OCCURENCES_LUT_LEN] {
-    let possible_count_occurences: [(u32, HandKind); 7] = [
-        (0x10000c, HandKind::FiveOfAKind),  // idx=16
-        (0x01001b, HandKind::FourOfAKind),  // idx=8
-        (0x00110b, HandKind::FullHouse),    // idx=5
-        (0x00102a, HandKind::ThreeOfAKind), // idx=4
-        (0x00021a, HandKind::TwoPairs),     // idx=2
-        (0x000139, HandKind::OnePair),      // idx=1
-        (0x000058, HandKind::FiveUnique),   // idx=0
-    ];
-    let mut lut = [HandKind::FiveUnique; OCCURENCES_LUT_LEN];
-
-    let mut i = 0;
-    while i < possible_count_occurences.len() {
-        let (occurences, kind) = possible_count_occurences[i];
-
-        let mut num_jokers = 0;
-        while num_jokers <= 5 {
-            let mut idx = count_occurences_to_unique_idx(occurences as usize) as usize;
-            idx = (idx << 3) + num_jokers;
-
-            let mut kind = kind;
-
-            if joker_is_wildcard {
-                match kind {
-                    HandKind::FourOfAKind => {
-                        if num_jokers > 0 {
-                            kind = HandKind::FiveOfAKind;
-                        }
-                    }
-                    HandKind::FullHouse => {
-                        if num_jokers > 0 {
-                            kind = HandKind::FiveOfAKind;
-                        }
-                    }
-                    HandKind::ThreeOfAKind => {
-                        if num_jokers > 0 {
-                            kind = HandKind::FourOfAKind;
-                        }
-                    }
-                    HandKind::TwoPairs => {
-                        if num_jokers == 1 {
-                            kind = HandKind::FullHouse;
-                        } else if num_jokers == 2 {
-                            kind = HandKind::FourOfAKind;
-                        }
-                    }
-                    HandKind::OnePair => {
-                        if num_jokers == 1 {
-                            kind = HandKind::ThreeOfAKind;
-                        } else if num_jokers == 2 {
-                            kind = HandKind::ThreeOfAKind;
-                        }
-                    }
-                    HandKind::FiveUnique => {
-                        if num_jokers == 1 {
-                            kind = HandKind::OnePair;
-                        }
-                    }
-                    _ => {}
-                }
-            }
-
-            lut[idx as usize] = kind;
-
-            num_jokers += 1;
-        }
-
-        i += 1;
-    }
-
-    lut
-}
-
-const fn count_occurences_to_unique_idx(mut occurences: usize) -> usize {
-    occurences >>= 8;
-    (occurences & 0xf)
-        + ((occurences & 0xf0) >> 2)
-        + ((occurences & 0xf00) >> 5)
-        + ((occurences & 0xf000) >> 8)
-}
-
-fn count_occurences_to_hand_kind(
-    occurences: usize,
-    num_jokers: usize,
-    occurences_lut: &[HandKind; OCCURENCES_LUT_LEN],
-) -> HandKind {
-    let mut idx = count_occurences_to_unique_idx(occurences) as usize;
-    idx = (idx << 3) + num_jokers;
-    occurences_lut[idx]
 }
 
 #[cfg(test)]
---
+++ 2023/day-7/src/lut.rs
@@ -0,0 +1,104 @@
+use crate::*;
+
+pub const LABEL_TO_STRENGTH_LUT_0: [u32; CARD_LUT_LEN] =
+    generate_label_to_strength_lut(b"23456789TJQKA");
+pub const LABEL_TO_STRENGTH_LUT_1: [u32; CARD_LUT_LEN] =
+    generate_label_to_strength_lut(b"J23456789TQKA");
+
+const fn generate_label_to_strength_lut(labels: &[u8]) -> [u32; CARD_LUT_LEN] {
+    let mut map = [u32::MAX; CARD_LUT_LEN];
+
+    let mut i = 0;
+    while i < labels.len() {
+        map[labels[i] as usize - SMALLEST_LABEL] = i as u32;
+        i += 1;
+    }
+
+    map
+}
+
+pub const OCCURENCES_LUT_LEN: usize = (16 << 3) + 5 + 1;
+pub const COUNT_OCCURENCES_TO_HAND_KIND_LUT_0: [HandKind; OCCURENCES_LUT_LEN] =
+    generate_count_occurences_to_hand_kind_lut(false);
+pub const COUNT_OCCURENCES_TO_HAND_KIND_LUT_1: [HandKind; OCCURENCES_LUT_LEN] =
+    generate_count_occurences_to_hand_kind_lut(true);
+
+const fn generate_count_occurences_to_hand_kind_lut(
+    joker_is_wildcard: bool,
+) -> [HandKind; OCCURENCES_LUT_LEN] {
+    let possible_count_occurences: [(u32, HandKind); 7] = [
+        (0x10000c, HandKind::FiveOfAKind),  // idx=16
+        (0x01001b, HandKind::FourOfAKind),  // idx=8
+        (0x00110b, HandKind::FullHouse),    // idx=5
+        (0x00102a, HandKind::ThreeOfAKind), // idx=4
+        (0x00021a, HandKind::TwoPairs),     // idx=2
+        (0x000139, HandKind::OnePair),      // idx=1
+        (0x000058, HandKind::FiveUnique),   // idx=0
+    ];
+    let mut lut = [HandKind::FiveUnique; OCCURENCES_LUT_LEN];
+
+    let mut i = 0;
+    while i < possible_count_occurences.len() {
+        let (occurences, kind) = possible_count_occurences[i];
+
+        let mut num_jokers = 0;
+        while num_jokers <= 5 {
+            let idx = count_occurences_to_lut_idx(occurences as usize) + num_jokers;
+            let mut kind = kind;
+
+            if joker_is_wildcard {
+                match kind {
+                    HandKind::FourOfAKind => {
+                        if num_jokers > 0 {
+                            kind = HandKind::FiveOfAKind;
+                        }
+                    }
+                    HandKind::FullHouse => {
+                        if num_jokers > 0 {
+                            kind = HandKind::FiveOfAKind;
+                        }
+                    }
+                    HandKind::ThreeOfAKind => {
+                        if num_jokers > 0 {
+                            kind = HandKind::FourOfAKind;
+                        }
+                    }
+                    HandKind::TwoPairs => {
+                        if num_jokers == 1 {
+                            kind = HandKind::FullHouse;
+                        } else if num_jokers == 2 {
+                            kind = HandKind::FourOfAKind;
+                        }
+                    }
+                    HandKind::OnePair => {
+                        if num_jokers == 1 {
+                            kind = HandKind::ThreeOfAKind;
+                        } else if num_jokers == 2 {
+                            kind = HandKind::ThreeOfAKind;
+                        }
+                    }
+                    HandKind::FiveUnique => {
+                        if num_jokers == 1 {
+                            kind = HandKind::OnePair;
+                        }
+                    }
+                    _ => {}
+                }
+            }
+
+            lut[idx as usize] = kind;
+
+            num_jokers += 1;
+        }
+
+        i += 1;
+    }
+
+    lut
+}
+
+pub const fn count_occurences_to_lut_idx(occurences: usize) -> usize {
+    let mut idx = occurences >> 8;
+    idx = (idx & 0xf) + ((idx & 0xf0) >> 2) + ((idx & 0xf00) >> 5) + ((idx & 0xf000) >> 8);
+    idx << 3
+}
