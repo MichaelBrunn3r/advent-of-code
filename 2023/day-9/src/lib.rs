3e77d161d8f92ae912870a8838c5995608bbd298
Michael Brunner <michaelbrunn3r@gmail.com>
2023-12-09 15:30:58 +0100

    ref: Use same solution for 1&2, only reversed order

--- 2023/day-9/src/lib.rs
+++ 2023/day-9/src/lib.rs
@@ -1,61 +1,52 @@
-use aoc::prelude::*;
 use itertools::Itertools;
-use regex::Regex;
 
 pub fn part_1(input: &str) -> i32 {
-    let mut predictions = vec![];
-
-    for line in input.lines() {
-        let (_, last_values) = collect_first_and_last_values(line);
-        predictions.push(last_values.iter().fold(0, |acc, f| f + acc));
-    }
-
-    predictions.iter().sum()
+    input
+        .lines()
+        .map(|line| predict_next_value(line.split(' ').map(|num| num.parse::<i32>().unwrap())))
+        .sum()
 }
 
 pub fn part_2(input: &str) -> i32 {
-    let mut predictions = vec![];
-
-    for line in input.lines() {
-        let (first_values, _) = collect_first_and_last_values(line);
-        let prediction = first_values.iter().rev().fold(0, |acc, f| f - acc);
-        predictions.push(prediction);
-    }
-
-    predictions.iter().sum()
+    input
+        .lines()
+        .map(|line| {
+            predict_next_value(line.split(' ').rev().map(|num| num.parse::<i32>().unwrap()))
+        })
+        .sum()
 }
 
-fn calc_differences(series: Vec<i32>) -> (i32, i32, bool, Vec<i32>) {
-    let last = series[series.len() - 1];
-    let first = series[0];
+fn predict_next_value(series: impl Iterator<Item = i32>) -> i32 {
+    let first_values = collect_first_and_last_values(series);
+    first_values.iter().fold(0, |acc, f| f + acc)
+}
+
+fn calc_differences(series: impl Iterator<Item = i32>) -> (i32, bool, Vec<i32>) {
     let mut all_zero = true;
+    let mut last = 0;
 
-    let mut diffs = vec![];
+    let diffs = series
+        .inspect(|x| last = *x)
+        .tuple_windows()
+        .map(|(a, b)| {
+            let diff = b - a;
+            all_zero &= diff == 0;
+            diff
+        })
+        .collect_vec();
 
-    for pair in series.windows(2) {
-        let diff = pair[1] - pair[0];
-        diffs.push(diff);
-        all_zero &= diff == 0;
-    }
-
-    (first, last, all_zero, diffs)
+    (last, all_zero, diffs)
 }
 
-fn collect_first_and_last_values(line: &str) -> (Vec<i32>, Vec<i32>) {
-    let (mut first, mut last, mut all_zero, mut diffs) = calc_differences(
-        line.split(' ')
-            .map(|num| num.parse::<i32>().unwrap())
-            .collect_vec(),
-    );
+fn collect_first_and_last_values(series: impl Iterator<Item = i32>) -> Vec<i32> {
+    let (mut last, mut all_zero, mut diffs) = calc_differences(series);
 
-    let mut first_values = vec![first];
     let mut last_values = vec![last];
 
     while !all_zero {
-        (first, last, all_zero, diffs) = calc_differences(diffs);
+        (last, all_zero, diffs) = calc_differences(diffs.into_iter());
         last_values.push(last);
-        first_values.push(first);
     }
 
-    (first_values, last_values)
+    last_values
 }
