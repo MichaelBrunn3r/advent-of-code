35388ef0ba0924510a39f00cdb252338b2ee6990
Michael Brunner <michaelbrunn3r@gmail.com>
2023-12-09 13:25:18 +0100

    ref: Add benchmarks to 2023-1

--- 2023/day-1/src/main.rs
+++ 2023/day-1/src/main.rs
@@ -1,114 +1,22 @@
-use lazy_static::lazy_static;
-use regex::Regex;
-use std::fs;
-use std::path::PathBuf;
-
-lazy_static! {
-    static ref PROJECT_DIR: PathBuf = PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap());
-}
+use aoc_2023_1::*;
 
 fn main() {
-    let input = fs::read_to_string(PROJECT_DIR.join("input")).unwrap();
-    println!(
-        "Sum of calibration values={}",
-        sum_of_calibration_values(input.as_str())
-    );
-}
-
-fn sum_of_calibration_values(input: &str) -> u32 {
-    input
-        .lines()
-        .map(|line| {
-            let (first, last) = first_and_last_digit(line.as_bytes());
-            10 * first + last
-        })
-        .sum()
-}
-
-lazy_static! {
-    static ref DIGIT_PATTERN: Regex = Regex::new(r"(\d)").unwrap();
-}
-
-fn first_and_last_digit(line: &[u8]) -> (u32, u32) {
-    let mut first = -1i32;
-    let mut last = -1i32;
-    for (i, c) in line.iter().enumerate() {
-        match c {
-            b'0'..=b'9' => {
-                last = (c - b'0') as i32;
-            }
-            b'e' => {
-                if &line[i..(i + 5).min(line.len())] == b"eight" {
-                    last = 8;
-                }
-            }
-            b'f' => {
-                if &line[i..(i + 4).min(line.len())] == b"five" {
-                    last = 5;
-                } else if &line[i..(i + 4).min(line.len())] == b"four" {
-                    last = 4;
-                }
-            }
-            b'n' => {
-                if &line[i..(i + 4).min(line.len())] == b"nine" {
-                    last = 9;
-                }
-            }
-            b'o' => {
-                if &line[i..(i + 3).min(line.len())] == b"one" {
-                    last = 1;
-                }
-            }
-            b's' => {
-                if &line[i..(i + 5).min(line.len())] == b"seven" {
-                    last = 7;
-                } else if &line[i..(i + 3).min(line.len())] == b"six" {
-                    last = 6;
-                }
-            }
-            b't' => {
-                if &line[i..(i + 3).min(line.len())] == b"two" {
-                    last = 2;
-                } else if &line[i..(i + 5).min(line.len())] == b"three" {
-                    last = 3;
-                }
-            }
-            _ => {}
-        }
-        if first < 0 {
-            first = last;
-        }
-    }
-
-    (first as u32, last as u32)
+    let input = aoc::read_input_to_string();
+    println!("Part 1: {}", part_1(&input));
+    println!("Part 2: {}", part_2(&input));
 }
 
 #[cfg(test)]
-mod test {
+mod tests {
     use super::*;
 
     #[test]
-    fn test_sum_of_calibration_values() {
-        assert_eq!(
-            sum_of_calibration_values(
-                r#"1abc2
-                pqr3stu8vwx
-                a1b2c3d4e5f
-                treb7uchet"#
-            ),
-            142
-        );
-        assert_eq!(
-            sum_of_calibration_values(
-                r#"two1nine
-                eightwothree
-                abcone2threexyz
-                xtwone3four
-                4nineeightseven2
-                zoneight234
-                7pqrstsixteen"#
-            ),
-            281
-        );
+    fn test_example_0() {
+        aoc::assert_solution(0, part_1);
+    }
+
+    #[test]
+    fn test_example_1() {
+        aoc::assert_solution(1, part_2);
     }
 }
