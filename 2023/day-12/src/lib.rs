40b0f895dddb173cc977a9cec9d6950c481b60e1
Michael Brunner <michaelbrunn3r@gmail.com>
2023-12-13 00:23:34 +0100

    solution: 2023-12

--- 2023/day-12/src/lib.rs
+++ 2023/day-12/src/lib.rs
@@ -1,162 +1,61 @@
-use std::ops::Range;
-
 use aoc::prelude::*;
 use itertools::Itertools;
 use regex::Regex;
 
+const OPERATIONAL: u8 = b'.';
+const DAMAGED: u8 = b'#';
+const UNKOWN: u8 = b'?';
+
 pub fn part_1(input: &str) -> usize {
-    // let rows = input.lines().map(|line| {
-    //     let (conditions, groups) = line.split_once(' ').unwrap();
-    //     let groups: Vec<usize> = groups
-    //         .split(',')
-    //         .map(|g| g.parse().unwrap())
-    //         .sorted()
-    //         .rev()
-    //         .collect();
-    //     let ranges = group_ranges(conditions.as_bytes());
-    //     (ranges, groups)
-    // });
-
-    let rows: Vec<(Vec<&[u8]>, Vec<usize>)> = vec![(vec![b"????", b"ABC"], vec![3, 2])];
-    let rows = rows.into_iter();
-
-    for (ranges, groups) in rows.skip(0).take(1) {
-        count_arrangements(0, &groups, ranges);
-    }
-
-    0
-}
-
-fn count_arrangements(group_idx: usize, groups: &[usize], initial_ranges: Vec<&[u8]>) -> usize {
-    // Base case if groups or ranges are empty
-    if groups.is_empty() {
-        let num_arrangements = !(groups.is_empty() ^ initial_ranges.is_empty()) as usize;
-        _out(group_idx, num_arrangements);
-        return num_arrangements;
-    }
-
-    _in(group_idx, groups[0], &initial_ranges);
-
-    let group = groups[0];
-    let next_group = *groups.get(1).unwrap_or(&usize::MAX);
-    let ranges_ge_group = initial_ranges
-        .iter()
-        .positions(|r| r.len() >= group)
-        .map(|i| (i, initial_ranges[i]));
-
-    let mut num_arrangements = 0;
-    for (range_idx, range) in ranges_ge_group {
-        _try(group_idx, range);
-        let ranges = clone_without(&initial_ranges, range_idx);
-
-        if range.len() == group {
-            let count = count_arrangements(group_idx + 1, &groups[1..], ranges);
-            if group == next_group {
-                num_arrangements = count;
-            } else {
-                num_arrangements += count;
-            }
-            continue;
-        }
-
-        // if range.len() == group + 1 {
-        //     let count = count_arrangements(group_idx + 1, &groups[1..], ranges);
-        //     if group == next_group {
-        //         num_arrangements += count;
-        //     } else {
-        //         num_arrangements += 2 * count;
-        //     }
-        //     continue;
-        // }
-
-        for start in 0..(range.len() - group + 1) {
-            let end = (start + group + 1).min(range.len());
-            let start = start.saturating_sub(1);
-
-            let subrange = &range[start..end];
-            _subrange(group_idx, subrange);
-
-            let mut sub_ranges = ranges.clone();
-
-            let (left, right) = range.split_at_range_unchecked(&(start..end));
-            println!(
-                "{}│──left {:?} ,right: {:?}",
-                "│  ".repeat(group_idx),
-                rts(left),
-                rts(right)
-            );
-
-            if !left.is_empty() {
-                sub_ranges.push(left);
-            }
-            if !right.is_empty() {
-                sub_ranges.push(right);
-            }
-
-            num_arrangements += count_arrangements(group_idx + 1, &groups[1..], sub_ranges);
-        }
-    }
-
-    _out(group_idx, num_arrangements);
-    num_arrangements
+    input
+        .lines()
+        .map(|line| {
+            let (springs, groups) = line.split_once(' ').unwrap();
+            let damaged_runs: Vec<usize> = groups.split(',').map(|g| g.parse().unwrap()).collect();
+            (springs.as_bytes(), damaged_runs)
+        })
+        .map(|(springs, damaged_runs)| count_arrangements(springs, &damaged_runs))
+        .sum()
 }
 
 pub fn part_2(input: &str) -> usize {
     0
 }
 
-fn group_ranges(conditions: &[u8]) -> Vec<&[u8]> {
-    let mut possible_ranges = vec![];
-
-    let mut start = 0;
-    while start < conditions.len() {
-        if conditions[start] == b'.' {
-            start += 1;
-            continue;
+fn count_arrangements(springs: &[u8], damaged_runs: &[usize]) -> usize {
+    // No groups left
+    if damaged_runs.is_empty() {
+        if springs.iter().any(|c| *c == DAMAGED) {
+            // There are some damaged springs left -> this arrangement is invalid
+            return 0;
+        } else {
+            // All springs are operational or unknown -> this arrangement is valid
+            return 1;
         }
-
-        let mut end = start + 1;
-        while end < conditions.len() && conditions[end] != b'.' {
-            end += 1;
-        }
-        possible_ranges.push(&conditions[start..end]);
-        start = end + 1;
     }
 
-    possible_ranges
-}
+    if springs.len() < damaged_runs.iter().sum::<usize>() + damaged_runs.len() - 1 {
+        return 0;
+    }
 
-fn rts(range: &[u8]) -> &str {
-    range.as_str_unchecked()
-}
+    if springs[0] == OPERATIONAL {
+        return count_arrangements(&springs[1..], damaged_runs);
+    }
 
-fn srts<'a>(ranges: &'a [&[u8]]) -> Vec<&'a str> {
-    ranges.iter().map(|r| rts(r)).collect()
-}
+    let mut num_arrangements = 0;
+    let run = damaged_runs[0];
+    let all_not_operational = springs[..run].iter().all(|c| *c != OPERATIONAL);
+    let end = (run + 1).min(springs.len());
 
-fn clone_without<'a>(vec: &'a Vec<&[u8]>, idx: usize) -> Vec<&'a [u8]> {
-    let mut vec = vec.clone();
-    vec.swap_remove(idx);
-    vec
-}
+    if all_not_operational
+        && ((springs.len() > run && springs[run] != DAMAGED) || springs.len() <= run)
+    {
+        num_arrangements = count_arrangements(&springs[end..], &damaged_runs[1..]);
+    }
 
-fn _in(idx: usize, group: usize, initial_ranges: &Vec<&[u8]>) {
-    println!(
-        "{}{}─in {:?}",
-        "│  ".repeat(idx),
-        group,
-        srts(initial_ranges)
-    );
-}
+    if springs[0] == UNKOWN {
+        num_arrangements += count_arrangements(&springs[1..], damaged_runs);
+    }
 
-fn _try(idx: usize, range: &[u8]) {
-    println!("{}├try {:?}", "│  ".repeat(idx), rts(range));
-}
-
-fn _out(idx: usize, count: usize) {
-    println!("{}└out {}", "│  ".repeat(idx), count);
-}
-
-fn _subrange(idx: usize, subrange: &[u8]) {
-    println!("{}│─sub {:?}", "│  ".repeat(idx), rts(subrange));
+    num_arrangements
 }
--- 2023/day-12/src/main.rs
+++ 2023/day-12/src/main.rs
@@ -2,8 +2,7 @@ use aoc_2023_12::*;
 
 fn main() {
     let input = aoc::read_input_to_string();
-    // println!("Part 1: {}", part_1(&input));
-    println!("Part 1: {}", part_1(&aoc::read_example_to_string(0)));
+    println!("Part 1: {}", part_1(&input));
     println!("Part 2: {}", part_2(&input));
 }
 
@@ -19,5 +18,11 @@ mod tests {
     #[test]
     fn test_example_1() {
         aoc::assert_solution(1, part_2);
+    }
+
+    #[test]
+    fn test_part_1() {
+        let input = aoc::read_input_to_string();
+        assert_eq!(part_1(&input), 7090);
     }
 }
