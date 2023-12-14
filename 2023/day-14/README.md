87c80103e3e12f8b7a66b40a0b4f868e5255e2fe
Michael Brunner <michaelbrunn3r@gmail.com>
2023-12-14 13:28:19 +0100

    perf: Iter over bytes instead of chars

--- 2023/day-14/README.md
+++ 2023/day-14/README.md
@@ -2,7 +2,7 @@
 
 ## Benchmarks (i5-1240P, with parsing, no I/O)
 
-- Part 1: `12.021 µs`
+- Part 1: `7.6281 µs`
 - Part 2: ???
 
 ## Explanation
--- 2023/day-14/src/lib.rs
+++ 2023/day-14/src/lib.rs
@@ -2,19 +2,20 @@ use aoc::prelude::*;
 use itertools::Itertools;
 use regex::Regex;
 
-const ROUND_ROCK: char = 'O';
-const CUBE_ROCK: char = '#';
-const EMPTY: char = '.';
+const ROUND_ROCK: u8 = b'O';
+const CUBE_ROCK: u8 = b'#';
+const EMPTY: u8 = b'.';
 
 pub fn part_1(input: &str) -> usize {
     let num_cols = input.lines().next().unwrap().len();
     let num_rows = input.lines().count();
+
     let mut current_stops = vec![num_rows + 1; num_cols];
     let mut total_load = 0;
 
     for (line, load) in input.lines().zip((1..num_rows + 1).rev()) {
-        for (col, c) in line.chars().enumerate() {
-            match c {
+        for (col, c) in line.as_bytes().iter().enumerate() {
+            match *c {
                 ROUND_ROCK => {
                     current_stops[col] -= 1;
                     total_load += current_stops[col];
