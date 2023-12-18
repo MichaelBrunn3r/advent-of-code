c473acdc3601ac8cd860c61ba7b2030cf207f2a7
Michael Brunner <michaelbrunn3r@gmail.com>
2023-12-18 23:11:20 +0100

    Attempt 2

--- 2023/day-17/src/lib.rs
+++ 2023/day-17/src/lib.rs
@@ -2,33 +2,31 @@ use std::collections::VecDeque;
 
 use aoc::prelude::*;
 use itertools::Itertools;
-use regex::Regex;
 
+// 1000 too low
+// 1100 too high
 // 1104 too high
 pub fn part_1(input: &mut str) -> usize {
-    let height = input.find('\n').unwrap();
-    let width = height + 1;
-    let map = unsafe { input.as_bytes_mut() };
+    let size = input.find('\n').unwrap();
 
-    // Convert ASCII numbers to integers. '\n' becomes 218
-    map.chunks_exact_mut(height).for_each(|line| {
-        for c in line.iter_mut() {
-            *c = c.wrapping_sub(b'0');
-        }
-    });
+    // Convert numbers to integers
+    let map = unsafe { input.as_bytes_mut() }
+        .chunks_exact_mut(size + 1)
+        .map(|line| line[0..size].iter().map(|b| b - b'0').collect_vec())
+        .collect_vec();
 
     let start = (0usize, 0usize);
-    let goal = (height - 1, width - 2);
-    let mut paths = VecDeque::from([Path::new(start)]);
+    let goal = (size - 1, size - 1);
+
+    let mut paths = vec![Path::new(start)];
 
     let mut iter = 0;
     let mut res = None;
     while !paths.is_empty() {
         iter += 1;
-        let path = paths.pop_front().unwrap();
+        let path = paths.swap_remove(0);
         let last = *path.last();
         if last == goal {
-            // println!("Found path: {:?}", path);
             res = Some(path);
             break;
         }
@@ -38,15 +36,13 @@ pub fn part_1(input: &mut str) -> usize {
         for i in (1..=3).rev() {
             if path.last_dir != Direction::Left && path.last_dir != Direction::Right {
                 // Expand right
-                if last.1 + i < width {
+                if last.1 + i < size {
                     let mut p = path.clone();
                     let next = (last.0, last.1 + i);
                     // Check for loop
                     if !p.visits(&next) {
-                        let heat_loss = (1..=i)
-                            .map(|j| map[last.0 * width + (last.1 + j)] as usize)
-                            .sum();
-                        p.add(next, i, heat_loss, Direction::Right);
+                        let cost = (1..=i).map(|j| map[last.0][last.1 + j] as usize).sum();
+                        p.add(next, cost, next.manhattan_distance(goal), Direction::Right);
                         expanded_paths.push(p);
                     }
                 }
@@ -57,10 +53,13 @@ pub fn part_1(input: &mut str) -> usize {
                     let next = (last.0, last.1 - i);
                     // Check for loop
                     if !p.visits(&next) {
-                        let heat_loss = (1..=i)
-                            .map(|j| map[last.0 * width + (last.1 - j)] as usize)
-                            .sum();
-                        p.add(next, i, heat_loss, Direction::Left);
+                        let cost = (1..=i).map(|j| map[last.0][last.1 - j] as usize).sum();
+                        p.add(
+                            next,
+                            cost,
+                            next.manhattan_distance(goal) + 1,
+                            Direction::Left,
+                        );
                         expanded_paths.push(p);
                     }
                 }
@@ -68,15 +67,13 @@ pub fn part_1(input: &mut str) -> usize {
 
             if path.last_dir != Direction::Up && path.last_dir != Direction::Down {
                 // Expand down
-                if last.0 + i < height {
+                if last.0 + i < size {
                     let mut p = path.clone();
                     let next = (last.0 + i, last.1);
                     // Check for loop
                     if !p.visits(&next) {
-                        let heat_loss = (1..=i)
-                            .map(|j| map[(last.0 + j) * width + last.1] as usize)
-                            .sum();
-                        p.add(next, i, heat_loss, Direction::Down);
+                        let cost = (1..=i).map(|j| map[last.0 + j][last.1] as usize).sum();
+                        p.add(next, cost, next.manhattan_distance(goal), Direction::Down);
                         expanded_paths.push(p);
                     }
                 }
@@ -87,52 +84,40 @@ pub fn part_1(input: &mut str) -> usize {
                     let next = (last.0 - i, last.1);
                     // Check for loop
                     if !p.visits(&next) {
-                        let heat_loss = (1..=i)
-                            .map(|j| map[(last.0 - j) * width + last.1] as usize)
-                            .sum();
-                        p.add(next, i, heat_loss, Direction::Up);
+                        let cost = (1..=i).map(|j| map[last.0 - j][last.1] as usize).sum();
+                        p.add(next, cost, next.manhattan_distance(goal) + 1, Direction::Up);
                         expanded_paths.push(p);
                     }
                 }
             }
         }
 
+        // If path that ends in the same node exists, keep the one with the lowest cost
         expanded_paths.retain(|p| {
             let last = *p.last();
             if let Some(other_pos) = paths.iter().position(|p| *p.last() == last) {
                 let other = paths.get(other_pos).unwrap();
-                return p.heat_loss < other.heat_loss;
-            }
+                return p.cost < other.cost;
+            };
             true
         });
 
         paths.extend(expanded_paths);
 
-        paths = paths
-            .into_iter()
-            .sorted_by_key(|p| {
-                let last = *p.last();
-                let dist = last.manhattan_distance(goal);
-                let rest_distance = match p.last_dir {
-                    Direction::Up | Direction::Left => dist + 2,
-                    _ => dist,
-                };
-                p.heat_loss + rest_distance
-            })
-            .collect();
+        paths.sort_by_key(|p| p.cost + p.estimated_rest_cost);
 
         if iter % 10_000 == 0 {
             println!("Iterations: {}", iter);
-            pretty_print(&map, &paths[0], width);
+            pretty_print(&map, &paths[0]);
         }
     }
 
     let res = res.unwrap();
-    pretty_print(&map, &res, width);
+    pretty_print(&map, &res);
     println!("Iterations: {}", iter);
     println!("Path: {:?}", res);
 
-    res.heat_loss
+    res.cost
 }
 
 const AVG_HEAT_LOSS: usize = 5;
@@ -141,39 +126,42 @@ pub fn part_2(input: &str) -> usize {
     0
 }
 
-fn pretty_print(map: &[u8], path: &Path, width: usize) {
-    let mut pretty = Vec::from(
-        map.iter()
-            .map(|b| if *b == 218 { b'\n' } else { b'.' })
-            .collect_vec(),
-    );
+fn pretty_print(map: &[Vec<u8>], path: &Path) {
+    let mut pretty = vec![vec![b'.'; map.len()]; map.len()];
+
     path.nodes.iter().tuple_windows().for_each(|(prev, curr)| {
         if prev.0 < curr.0 {
             for i in prev.0 + 1..=curr.0 {
-                pretty[i * width + prev.1] = b'v';
+                pretty[i][prev.1] = b'v';
             }
         } else if prev.0 > curr.0 {
             for i in curr.0..prev.0 {
-                pretty[i * width + prev.1] = b'^';
+                pretty[i][prev.1] = b'^';
             }
         } else if prev.1 < curr.1 {
             for i in prev.1 + 1..=curr.1 {
-                pretty[prev.0 * width + i] = b'>';
+                pretty[prev.0][i] = b'>';
             }
         } else if prev.1 > curr.1 {
             for i in curr.1..prev.1 {
-                pretty[prev.0 * width + i] = b'<';
+                pretty[prev.0][i] = b'<';
             }
         }
     });
-    println!("{}", String::from_utf8(pretty).unwrap());
+    println!(
+        "{}",
+        pretty
+            .iter()
+            .map(|line| String::from_utf8_lossy(line))
+            .join("\n")
+    );
 }
 
 #[derive(Debug, Clone)]
 struct Path {
     nodes: Vec<(usize, usize)>,
-    len: usize,
-    heat_loss: usize,
+    cost: usize,
+    estimated_rest_cost: usize,
     last_dir: Direction,
 }
 
@@ -181,9 +169,9 @@ impl Path {
     fn new(start: (usize, usize)) -> Self {
         Self {
             nodes: vec![start],
-            len: 0,
-            heat_loss: 0,
+            cost: 0,
             last_dir: Direction::None,
+            estimated_rest_cost: 0,
         }
     }
 
@@ -195,10 +183,16 @@ impl Path {
         self.nodes.contains(node)
     }
 
-    fn add(&mut self, node: (usize, usize), path_len: usize, heat_loss: usize, dir: Direction) {
+    fn add(
+        &mut self,
+        node: (usize, usize),
+        cost: usize,
+        estimated_rest_cost: usize,
+        dir: Direction,
+    ) {
         self.nodes.push(node);
-        self.len += path_len;
-        self.heat_loss += heat_loss;
+        self.cost += cost;
+        self.estimated_rest_cost = estimated_rest_cost;
         self.last_dir = dir;
     }
 }
