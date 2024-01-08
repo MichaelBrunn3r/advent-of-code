177b3af244be55f73e4e33871498dcf24bf0c222
Michael Brunner <michaelbrunn3r@gmail.com>
2024-01-08 11:42:42 +0100

    perf(23d24): More predictable interaction detection

--- 2023/day-24/src/lib.rs
+++ 2023/day-24/src/lib.rs
@@ -1,11 +1,6 @@
 use std::fmt::{Debug, Formatter};
 
-// static mut MOVING_LEFT: ConstVec<Hail>
-
-const MIN: usize = 11222456353998;
-const MAX: usize = 559318866468353;
-const BOUNDS: [usize; 2] = [200000000000000, 400000000000000];
-
+const BOUNDS: [f64; 2] = [200000000000000.0, 400000000000000.0];
 static mut HAIL_STONES: ConstVec<Hail, 300> = unsafe { std::mem::zeroed() };
 
 pub fn part_1(input: &str) -> usize {
@@ -14,39 +9,39 @@ pub fn part_1(input: &str) -> usize {
         HAIL_STONES.clear();
 
         while data.read() != b'\n' {
-            let mut pos = [0, 0, 0];
+            let mut pos = [0.0, 0.0, 0.0];
             for comp in &mut pos {
                 while data.read().is_ascii_digit() {
-                    *comp = *comp * 10 + (data.read() - b'0') as usize;
+                    *comp = *comp * 10.0 + (data.read() - b'0') as f64;
                     data = data.add(1);
                 }
 
                 data = data.add(", ".len());
             }
 
-            let mut vel = [0, 0, 0];
+            let mut vel = [0.0, 0.0, 0.0];
             for comp in &mut vel {
                 data = data.add(" ".len());
                 let sign = if data.read() == b'-' {
                     data = data.add(1);
-                    -1
+                    -1.0
                 } else {
-                    1isize
+                    1.0
                 };
 
                 while data.read().is_ascii_digit() {
-                    *comp = *comp * 10 + (data.read() - b'0') as isize;
+                    *comp = *comp * 10.0 + (data.read() - b'0') as f64;
                     data = data.add(1);
                 }
                 *comp *= sign;
                 data = data.add(",".len());
             }
 
-            let slope = vel[1] as f64 / vel[0] as f64;
+            let slope = vel[1] / vel[0];
             HAIL_STONES.push(Hail {
                 pos: [pos[0], pos[1]],
                 vel: [vel[0], vel[1]],
-                y_intercept: pos[1] as f64 - slope * pos[0] as f64,
+                y_intercept: pos[1] - slope * pos[0],
                 slope,
             });
         }
@@ -58,19 +53,17 @@ pub fn part_1(input: &str) -> usize {
                 let b = &HAIL_STONES.data[j];
 
                 let x = (a.y_intercept - b.y_intercept) / (b.slope - a.slope);
-                if x < BOUNDS[0] as f64 || x > BOUNDS[1] as f64 {
-                    continue;
-                }
-
-                let t_a = (x - a.pos[0] as f64) / a.vel[0] as f64;
-                let t_b = (x - b.pos[0] as f64) / b.vel[0] as f64;
-
-                if t_a < 0.0 || t_b < 0.0 {
-                    continue;
-                }
-
                 let y = a.slope * x + a.y_intercept;
-                if y < BOUNDS[0] as f64 || y > BOUNDS[1] as f64 {
+                let t_a = (x - a.pos[0]) / a.vel[0];
+                let t_b = (x - b.pos[0]) / b.vel[0];
+
+                if x < BOUNDS[0]
+                    || x > BOUNDS[1]
+                    || y < BOUNDS[0]
+                    || y > BOUNDS[1]
+                    || t_a < 0.0
+                    || t_b < 0.0
+                {
                     continue;
                 }
 
@@ -88,8 +81,8 @@ pub fn part_2(input: &str) -> usize {
 
 #[derive(Debug)]
 struct Hail {
-    pos: [usize; 2],
-    vel: [isize; 2],
+    pos: [f64; 2],
+    vel: [f64; 2],
     y_intercept: f64,
     slope: f64,
 }
@@ -100,13 +93,6 @@ struct ConstVec<T, const C: usize> {
 }
 
 impl<T, const C: usize> ConstVec<T, C> {
-    const fn new() -> Self {
-        Self {
-            data: unsafe { std::mem::zeroed() },
-            len: 0,
-        }
-    }
-
     fn push(&mut self, value: T) {
         self.data[self.len as usize] = value;
         self.len += 1;
@@ -114,11 +100,6 @@ impl<T, const C: usize> ConstVec<T, C> {
 
     fn clear(&mut self) {
         self.len = 0;
-    }
-
-    fn insert_as_last(&mut self, idx: u16, value: T) {
-        self.data[idx as usize] = value;
-        self.len = idx + 1;
     }
 }
 
