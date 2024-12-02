32d88defb539f1f6cc94c3ce0ff2b5667d4fd5dc
Michael Brunner <michaelbrunn3r@gmail.com>
2024-12-03 00:51:16 +0100

    perf(2024-2): Use LevelIterator in part 1

--- 2024/day-2/src/lib.rs
+++ 2024/day-2/src/lib.rs
@@ -8,33 +8,30 @@ pub fn part_1(input: &str) -> usize {
 
     let mut crs = input.as_ptr();
     'reports: for _ in 0..NUM_REPORTS {
-        let first = parse_level(&mut crs);
-        crs.take();
-        let mut prev = parse_level(&mut crs);
-        crs.take();
+        let mut levels = LevelIterator::new(&mut crs);
+
+        let first = levels.next().unwrap();
+        let mut prev = levels.next().unwrap();
 
         let increasing = first < prev;
         if first == prev || first.abs_diff(prev) > 3 {
-            skip_to_next_line(&mut crs);
             continue 'reports;
         }
 
         loop {
-            let level = parse_level(&mut crs);
+            let level = levels.next().unwrap();
 
             if level == prev
                 || level.abs_diff(prev) > 3
                 || (increasing && prev > level)
                 || (!increasing && prev < level)
             {
-                skip_to_next_line(&mut crs);
                 continue 'reports;
             }
             
-            let separator = crs.take();
             prev = level;
 
-            if separator == b'\n' {
+            if levels.last_separator == b'\n' {
                 break;
             }
         }
@@ -45,69 +42,74 @@ pub fn part_1(input: &str) -> usize {
     num_safe
 }
 
-fn parse_level(crs: &mut *const u8) -> u8 {
-    let mut num = crs.take() - b'0';    
-    if crs.peek() >= b'0' {
-        num *= 10;
-        num += crs.take() - b'0';
+pub fn part_2(input: &str) -> usize {
+    // input
+    //     .split("\n")
+    //     .filter(|line| !line.is_empty())
+    //     .map(|line| {
+    //         let levels = LevelIterator{crs: line.as_ptr()}.collect_vec();
+
+    //         for skipi in 0..levels.len() {
+    //             if levels[..skipi].iter().chain(levels[skipi+1..].iter())
+    //                 .tuple_windows()
+    //                 .map(|(a, b)| *a as i32 - *b as i32)
+    //                 .tuple_windows()
+    //                 .all(|(a, b)| {
+    //                     return (a >= 1 && a <= 3 && b >= 1 && b <= 3) 
+    //                         || (a <= -1 && a >= -3 && b <= -1 && b >= -3);
+    //                 })
+    //             {
+    //                 return true;
+    //             }
+    //         }
+
+    //         false
+    //     })
+    //     .filter(|line| *line)
+    //     .count()
+    0
+}
+
+struct LevelIterator<'a> {
+    crs: &'a mut *const u8,
+    last_separator: u8
+}
+
+impl<'a> LevelIterator<'a> {
+    fn new(crs: &'a mut *const u8) -> Self {
+        LevelIterator {
+            crs,
+            last_separator: 0
+        }
     }
 
-    num
+    fn flush(&mut self) {
+        if self.last_separator == b'\n' {
+            return;
+        }
+
+        while self.crs.take() != b'\n' {}
+    }
 }
 
-fn skip_to_next_line(crs: &mut *const u8) {
-    while crs.take() != b'\n' {}
+impl<'a> Drop for LevelIterator<'a> {
+    fn drop(&mut self) {
+        self.flush();
+    }
 }
 
-pub fn part_2(input: &str) -> usize {
-    input
-        .split("\n")
-        .filter(|line| !line.is_empty())
-        .map(|line| {
-            let levels = LevelIterator{crs: line.as_ptr()}.collect_vec();
-
-            for skipi in 0..levels.len() {
-                if levels[..skipi].iter().chain(levels[skipi+1..].iter())
-                    .tuple_windows()
-                    .map(|(a, b)| *a as i32 - *b as i32)
-                    .tuple_windows()
-                    .all(|(a, b)| {
-                        return (a >= 1 && a <= 3 && b >= 1 && b <= 3) 
-                            || (a <= -1 && a >= -3 && b <= -1 && b >= -3);
-                    })
-                {
-                    return true;
-                }
-            }
-
-            false
-        })
-        .filter(|line| *line)
-        .count()
-}
-
-struct LevelIterator {
-    crs: *const u8,
-}
-
-impl Iterator for LevelIterator {
+impl<'a> Iterator for LevelIterator<'a> {
     type Item = u8;
 
     fn next(&mut self) -> Option<u8> {
-        if self.crs.peek() == b'\n' {
-            return None;
-        }
-
-        let mut num = self.crs.take() - b'0';
+        let mut num = self.crs.take() - b'0';    
         if self.crs.peek() >= b'0' {
             num *= 10;
             num += self.crs.take() - b'0';
         }
 
-        if self.crs.peek() == b' ' {
-            self.crs.take();
-        }
+        self.last_separator = self.crs.take();
 
-        return Some(num);
+        return Some(num)
     }
 }
