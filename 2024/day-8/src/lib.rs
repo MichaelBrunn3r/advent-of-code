c6a767658527368f4c9dedda613b7d78d18a5d4d
Michael Brunner <michaelbrunn3r@gmail.com>
2024-12-08 13:30:33 +0100

    style: Format lib.rs

--- 2024/day-8/src/lib.rs
+++ 2024/day-8/src/lib.rs
@@ -4,7 +4,7 @@ use aoc::prelude::*;
 use itertools::Itertools;
 
 const SIDE_LENGTH: usize = 50;
-const LINE_LENGTH: usize = SIDE_LENGTH+1;
+const LINE_LENGTH: usize = SIDE_LENGTH + 1;
 type NodeLocations = Vec<Vec<(usize, usize)>>;
 
 pub fn parse(input: &str) -> NodeLocations {
@@ -20,75 +20,77 @@ pub fn parse(input: &str) -> NodeLocations {
                 .enumerate()
                 .filter(|(_, &c)| c != b'.')
                 .for_each(|(x, &c)| {
-                    node_locations[(c - b'0') as usize].push((x,y));
+                    node_locations[(c - b'0') as usize].push((x, y));
                 });
         });
 
     node_locations
-}   
+}
 
 pub fn p(node_locations: &NodeLocations) -> (usize, usize) {
     let mut antinode_locations = [0u64; SIDE_LENGTH];
     let mut antinode_locations_harmonics = [0u64; SIDE_LENGTH];
 
-    node_locations
-        .iter()
-        .for_each(|node_locations| {
-            node_locations
-                .iter()
-                .tuple_combinations()
-                .for_each(|(&a, &b)| {
-                    antinode_locations_harmonics[a.1] |= 1 << a.0;
-                    antinode_locations_harmonics[b.1] |= 1 << b.0;
+    node_locations.iter().for_each(|node_locations| {
+        node_locations
+            .iter()
+            .tuple_combinations()
+            .for_each(|(&a, &b)| {
+                antinode_locations_harmonics[a.1] |= 1 << a.0;
+                antinode_locations_harmonics[b.1] |= 1 << b.0;
 
-                    let dx = b.0 as i32 - a.0 as i32;
-                    let dy = b.1 as i32 - a.1 as i32;
+                let dx = b.0 as i32 - a.0 as i32;
+                let dy = b.1 as i32 - a.1 as i32;
 
-                    {
-                        let mut x = b.0 as i32 + dx;
-                        let mut y = b.1 as i32 + dy;
+                {
+                    let mut x = b.0 as i32 + dx;
+                    let mut y = b.1 as i32 + dy;
 
-                        if in_bounds(x,y) {
-                            antinode_locations[y as usize] |= 1 << x;
-                            loop {
-                                antinode_locations_harmonics[y as usize] |= 1 << x;
-    
-                                x += dx;
-                                y += dy;
-    
-                                if !in_bounds(x,y) {
-                                    break;
-                                }
+                    if in_bounds(x, y) {
+                        antinode_locations[y as usize] |= 1 << x;
+                        loop {
+                            antinode_locations_harmonics[y as usize] |= 1 << x;
+
+                            x += dx;
+                            y += dy;
+
+                            if !in_bounds(x, y) {
+                                break;
                             }
                         }
                     }
+                }
 
-                    {
-                        let mut x = a.0 as i32 - dx;
-                        let mut y = a.1 as i32 - dy;
+                {
+                    let mut x = a.0 as i32 - dx;
+                    let mut y = a.1 as i32 - dy;
 
-                        if in_bounds(x,y) {
-                            antinode_locations[y as usize] |= 1 << x;
-                            loop {
-                                antinode_locations_harmonics[y as usize] |= 1 << x;
-    
-                                x -= dx;
-                                y -= dy;
-    
-                                if !in_bounds(x,y) {
-                                    break;
-                                }
+                    if in_bounds(x, y) {
+                        antinode_locations[y as usize] |= 1 << x;
+                        loop {
+                            antinode_locations_harmonics[y as usize] |= 1 << x;
+
+                            x -= dx;
+                            y -= dy;
+
+                            if !in_bounds(x, y) {
+                                break;
                             }
                         }
-
-
                     }
-                });
-        });
+                }
+            });
+    });
 
     (
-        antinode_locations.iter().map(|bitmap| bitmap.count_ones() as usize).sum(),
-        antinode_locations_harmonics.iter().map(|bitmap| bitmap.count_ones() as usize).sum(),
+        antinode_locations
+            .iter()
+            .map(|bitmap| bitmap.count_ones() as usize)
+            .sum(),
+        antinode_locations_harmonics
+            .iter()
+            .map(|bitmap| bitmap.count_ones() as usize)
+            .sum(),
     )
 }
 
