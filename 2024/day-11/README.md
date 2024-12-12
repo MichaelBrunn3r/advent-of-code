8dc07f15a7dd9b92049dd048d0285189856b4292
Michael Brunner <michaelbrunn3r@gmail.com>
2024-12-12 10:48:52 +0100

    perf: Use DashMap

--- 2024/day-11/README.md
+++ 2024/day-11/README.md
@@ -3,9 +3,9 @@
 ## Benchmarks
 
 <!-- BEGIN benches -->
-| Benchmark            | Time     |
-| -------------------- | -------- |
-| [p](./src/lib.rs#L6) | 9.11 µs |
+| Benchmark             | Time     |
+| --------------------- | -------- |
+| [p](./src/lib.rs#L10) | 7.05 µs |
 
 <!-- END benches -->
 <!-- BEGIN other_benches -->
--- 2024/day-11/src/lib.rs
+++ 2024/day-11/src/lib.rs
@@ -1,29 +1,39 @@
 use aoc::prelude::*;
-use fxhash::FxHashMap;
-use memoize::memoize;
+use dashmap::DashMap;
+use lazy_static::lazy_static;
 use rayon::{iter::ParallelIterator, str::ParallelString};
+
+lazy_static! {
+    static ref MEMO: DashMap<(u8, usize), usize> = DashMap::new();
+}
 
 pub fn p(input: &str) -> (usize, usize) {
     input[..input.len() - 1]
         .par_split(' ')
         .map(|stone| stone.as_bytes().parse_ascii_digits())
         .map(|stone| (blink(25, stone), blink(75, stone)))
-        .reduce(|| (0,0), |a,b| (a.0+b.0,a.1+b.1))
+        .reduce(|| (0, 0), |a, b| (a.0 + b.0, a.1 + b.1))
 }
 
-#[memoize(CustomHasher: FxHashMap, HasherInit: FxHashMap::default(), SharedCache)]
 fn blink(n: u8, stone: usize) -> usize {
-    return if n == 0 {
+    if let Some(cached) = MEMO.get(&(n, stone)) {
+        return *cached;
+    }
+
+    let num_stones = if n == 0 {
         1
     } else if stone == 0 {
-        blink(n-1, 1)
+        blink(n - 1, 1)
     } else {
         let num_digits = stone.digits();
         if num_digits.even() {
             let split = 10usize.pow(num_digits as u32 / 2);
-            blink(n-1, stone / split) + blink(n-1, stone % split)
+            blink(n - 1, stone / split) + blink(n - 1, stone % split)
         } else {
-            blink(n-1, stone*2024)
+            blink(n - 1, stone * 2024)
         }
-    }
+    };
+
+    MEMO.insert((n, stone), num_stones);
+    num_stones
 }
