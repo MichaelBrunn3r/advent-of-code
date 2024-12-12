9725eb45070faf0f5d16c51819886454e31b4cc2
Michael Brunner <michaelbrunn3r@gmail.com>
2024-12-12 22:33:58 +0100

    fail: Attempt 2 with ray casting stuff

--- 2024/day-12/src/lib.rs
+++ 2024/day-12/src/lib.rs
@@ -8,10 +8,13 @@ use itertools::Itertools;
 
 const MASK_PLANT: u8 = 0b0001_1111;
 const FLAG_VISITED: u8 = 0b0010_0000;
-const LINE_LENGTH: usize = 6;
+const LINE_LENGTH: usize = 5;
 
 pub fn p1(input: &mut str) -> usize {
     let bytes = unsafe { input.as_bytes_mut() };
+    let line_length = bytes.iter().position(|&b| b == b'\n').unwrap() + 1;
+    let bytes_len = bytes.len();
+
     let mut total = 0;
 
     let mut unhandled_plots = vec![0usize];
@@ -20,33 +23,20 @@ pub fn p1(input: &mut str) -> usize {
         if plant & FLAG_VISITED != 0 || plant == b'\n' {
             continue;
         }
-
-        println!("{:?} {idx_start_plot}", plant as char);
-        let (mut inside, mut outside, sides, perimeter) = walk_perimiter(idx_start_plot, bytes);
-        let mut inside = inside
-            .drain()
-            .filter(|&idx| idx >= 0 && idx < bytes.len() as i32 && bytes[idx as usize] != b'\n')
-            .map(|idx| idx as usize)
-            .collect_vec();
-        unhandled_plots.append(&mut outside);
-
+        println!("{:?}", plant as char);
         bytes[idx_start_plot] |= FLAG_VISITED;
 
-        let mut edges = Vec::new();
-        let mut plot_area = perimeter;
-        // let mut stack = vec![idx_start_plot];
-        // let mut perimiter = 0;
-        while let Some(idx_current) = inside.pop() {
+        let mut plot_area = 0;
+        let mut stack = vec![idx_start_plot];
+        let mut edge = HashSet::new();
+        while let Some(idx_current) = stack.pop() {
             plot_area += 1;
-            let mut is_edge = false;
-
-            for idx_adjacent in [1i32, -1, LINE_LENGTH as i32, -(LINE_LENGTH as i32)]
+            for idx_adjacent in [1i32, -1, line_length as i32, -(line_length as i32)]
                 .iter()
                 .map(|&offset| idx_current as i32 + offset)
             {
-                if idx_adjacent < 0 || idx_adjacent >= bytes.len() as i32 {
-                    // perimiter += 1;
-                    is_edge = true;
+                if idx_adjacent < 0 || idx_adjacent >= bytes_len as i32 {
+                    edge.insert(idx_current);
                     continue;
                 }
 
@@ -54,34 +44,104 @@ pub fn p1(input: &mut str) -> usize {
                 let adjacent = bytes[idx_adjacent];
 
                 if adjacent == b'\n' {
-                    // perimiter += 1;
-                    is_edge = true;
+                    edge.insert(idx_current);
                     continue;
                 }
 
                 if (plant & MASK_PLANT) == (adjacent & MASK_PLANT) {
                     if adjacent & FLAG_VISITED == 0 {
-                        inside.push(idx_adjacent);
+                        stack.push(idx_adjacent);
                     }
                     bytes[idx_adjacent] |= FLAG_VISITED;
                 } else {
-                    // perimiter += 1;
-                    is_edge = true;
+                    edge.insert(idx_current);
                     if adjacent & FLAG_VISITED == 0 {
                         unhandled_plots.push(idx_adjacent);
                     }
                 }
             }
-
-            if is_edge {
-                edges.push(idx_current);
-            }
         }
 
-        println!("  area={plot_area}");
-        // edges.sort();
-        // println!("{} {:?}", plant as char, edges);
-        total += plot_area * sides;
+        let mut edge = edge
+            .drain()
+            .map(|idx| (idx % LINE_LENGTH, idx / LINE_LENGTH, idx))
+            .collect_vec();
+        edge.sort_by_key(|e| (e.1, e.0));
+        // println!("  {:?}", edge);
+
+        let mut prev_has_top_edge = false;
+        let mut prev_has_bottom_edge = false;
+        let mut sides_h: usize = edge
+            .iter()
+            .tuple_windows()
+            .map(|(a, b)| {
+                let mut sum = 0;
+                if b.1 != a.1 {
+                    sum += prev_has_top_edge as usize + prev_has_bottom_edge as usize;
+                }
+
+                if b.2 - a.2 != 1 {
+                    return 0;
+                }
+
+                prev_has_top_edge = is_edge(plant, b.2 as i32 - LINE_LENGTH as i32, bytes);
+                if is_edge(plant, a.2 as i32 - LINE_LENGTH as i32, bytes)
+                    != prev_has_top_edge
+                {
+                    sum += 1;
+                }
+
+                prev_has_bottom_edge = is_edge(plant, b.2 as i32 + LINE_LENGTH as i32, bytes);
+                if is_edge(plant, a.2 as i32 + LINE_LENGTH as i32, bytes)
+                    != prev_has_bottom_edge
+                {
+                    sum += 1;
+                }
+
+                sum
+            })
+            .sum::<usize>();
+        if prev_has_top_edge {
+            sides_h += 1;
+        }
+        if prev_has_bottom_edge {
+            sides_h += 1;
+        }
+        if sides_h == 0 && edge.len() == 1 {
+            sides_h = 2;
+        }
+
+        edge.sort_by_key(|e| (e.0, e.1));
+        let mut sides_v: usize = edge
+            .iter()
+            .tuple_windows()
+            .map(|(a, b)| {
+                if b.2 as i32 - a.2 as i32 != LINE_LENGTH as i32 {
+                    return 0;
+                }
+
+                let mut sum = 0;
+                if is_edge(plant, a.2 as i32 - 1 as i32, bytes)
+                    != is_edge(plant, b.2 as i32 - 1 as i32, bytes)
+                {
+                    sum += 1;
+                }
+
+                if is_edge(plant, a.2 as i32 + 1 as i32, bytes)
+                    != is_edge(plant, b.2 as i32 + 1 as i32, bytes)
+                {
+                    sum += 1;
+                }
+
+                sum
+            })
+            .sum();
+        if sides_v == 0 && edge.len() == 1 {
+            sides_v = 2;
+        }
+
+        println!("    {sides_h} {sides_v}");
+        total += plot_area * (sides_h + sides_v);
     }
 
     total
@@ -92,23 +152,24 @@ pub fn p2(input: &str) -> usize {
 }
 
 const ADJACENT_EDGE_DIRS: [(i32, Direction); 4] = [
-    (1, Direction::Right), // Right -> Right
+    (1, Direction::Right),                 // Right -> Right
     (LINE_LENGTH as i32, Direction::Down), // Below -> Down
-    (-1, Direction::Left), // Left -> Left
-    (LINE_LENGTH as i32, Direction::Up), // Above -> Up
+    (-1, Direction::Left),                 // Left -> Left
+    (LINE_LENGTH as i32, Direction::Up),   // Above -> Up
 ];
 
-pub fn walk_perimiter(idx_start: usize, bytes: &mut [u8]) -> (HashSet<i32>, Vec<usize>, usize, usize) {
+pub fn walk_perimiter(
+    idx_start: usize,
+    bytes: &mut [u8],
+) -> (HashSet<i32>, Vec<usize>, usize, usize) {
     let plant = bytes[idx_start];
-    let dir = ADJACENT_EDGE_DIRS
-        .iter()
-        .find_map(|(offset, dir)| {
-            let idx_adjacent = idx_start as i32 + offset;
-            if !is_edge(plant, idx_adjacent, bytes) {
-                return Some(dir.clone())
-            }
-            None
-        });
+    let dir = ADJACENT_EDGE_DIRS.iter().find_map(|(offset, dir)| {
+        let idx_adjacent = idx_start as i32 + offset;
+        if !is_edge(plant, idx_adjacent, bytes) {
+            return Some(dir.clone());
+        }
+        None
+    });
     if dir.is_none() {
         println!("  sides={}\n  permimeter={:?}", 4, 1);
         bytes[idx_start] |= FLAG_VISITED;
@@ -126,7 +187,10 @@ pub fn walk_perimiter(idx_start: usize, bytes: &mut [u8]) -> (HashSet<i32>, Vec<
     for _ in 0..20 {
         inside.remove(&(idx_current));
         outside.insert(idx_current + dir.edge_offset());
-        if idx_current == idx_start as i32 && dir == dir_start && bytes[idx_current as usize] & FLAG_VISITED != 0 {
+        if idx_current == idx_start as i32
+            && dir == dir_start
+            && bytes[idx_current as usize] & FLAG_VISITED != 0
+        {
             // if is_edge(plant, dir.walk(idx_current), bytes) {
             //     println!("  clockwise last");
             //     sides += 1;
@@ -177,14 +241,20 @@ pub fn walk_perimiter(idx_start: usize, bytes: &mut [u8]) -> (HashSet<i32>, Vec<
         .map(|idx| idx as usize)
         .collect_vec();
 
-    println!("  sides={}\n  permimeter={:?}\n  inside={:?}\n  outside={:?}", sides, perimeter.len(), inside, outside);
+    println!(
+        "  sides={}\n  permimeter={:?}\n  inside={:?}\n  outside={:?}",
+        sides,
+        perimeter.len(),
+        inside,
+        outside
+    );
     (inside, outside, sides, perimeter.len())
 }
 
-fn is_edge(plant: u8, idx_next: i32, bytes: &[u8]) -> bool {
-    idx_next < 0
-        || idx_next >= bytes.len() as i32
-        || (plant & MASK_PLANT) != (bytes[idx_next as usize] & MASK_PLANT)
+fn is_edge(plant: u8, idx_other: i32, bytes: &[u8]) -> bool {
+    idx_other < 0
+        || idx_other >= bytes.len() as i32
+        || (plant & MASK_PLANT) != (bytes[idx_other as usize] & MASK_PLANT)
 }
 
 #[derive(Debug, Clone, Copy, PartialEq)]
