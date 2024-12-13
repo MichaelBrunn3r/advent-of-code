1f6b34a270e311ad9869cc3a285e925656bc08d2
Michael Brunner <michaelbrunn3r@gmail.com>
2024-12-14 00:52:03 +0100

    tmp

--- 2024/day-13/src/main.rs
+++ 2024/day-13/src/main.rs
@@ -1,13 +1,7 @@
 use aoc_2024_13::*;
 
-static mut MACHINES: Machines = unsafe{std::mem::zeroed()};
-
 fn main() {
-    unsafe{
-        parse(&aoc::read_input_to_string(), &mut MACHINES);
-        println!("Part 1: {}", p1(&MACHINES));
-        println!("Part 2: {}", p2(&MACHINES));
-    }
+    println!("Part 1&2: {:?}", p(&aoc::read_input_to_string()));
 }
 
 #[cfg(test)]
@@ -15,16 +9,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_p1() {
-        let mut machines: Machines = unsafe{std::mem::zeroed()};
-        parse(&aoc::read_input_to_string(), &mut machines);
-        assert_eq!(p1(&machines), 29187);
-    }
-
-    #[test]
-    fn test_p2() {
-        let mut machines: Machines = unsafe{std::mem::zeroed()};
-        parse(&aoc::read_input_to_string(), &mut machines);
-        assert_eq!(p2(&machines), 99968222587852);
+    fn test_p() {
+        assert_eq!(p(&aoc::read_input_to_string()), (29187, 99968222587852));
     }
 }
