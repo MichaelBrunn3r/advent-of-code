703567ec1d276288e8aac9f532a7fd077e150908
Michael Brunner <michaelbrunn3r@gmail.com>
2024-12-19 13:39:14 +0100

    solve(2024-19): Part 2

--- 2024/day-19/src/lib.rs
+++ 2024/day-19/src/lib.rs
@@ -3,7 +3,7 @@ use std::collections::HashMap;
 use aoc::prelude::*;
 use itertools::Itertools;
 
-pub fn p1(input: &str) -> usize {
+pub fn p(input: &str) -> (usize, usize) {
     let (patterns, designs) = input.split_once("\n\n").unwrap();
     let patterns = patterns.split(", ").map(|s| s.as_bytes()).collect_vec();
     let mut memo = HashMap::new();
@@ -12,26 +12,34 @@ pub fn p1(input: &str) -> usize {
         .split("\n")
         .map(|d| d.as_bytes())
         .filter(|d| d.len() > 0)
-        .filter(|d| is_possible(&patterns, d, &mut memo))
-        .count()
+        .map(|d| num_possibilities(&patterns, d, &mut memo))
+        .filter(|&n| n > 0)
+        .map(|n| (1, n))
+        .reduce(|a, b| (a.0 + b.0, a.1 + b.1))
+        .unwrap()
 }
 
-pub fn p2(input: &str) -> usize {
-    0
-}
-
-fn is_possible<'d>(patterns: &[&[u8]], design: &'d [u8], memo: &mut HashMap<&'d [u8], bool>) -> bool {
+fn num_possibilities<'d>(
+    patterns: &[&[u8]],
+    design: &'d [u8],
+    memo: &mut HashMap<&'d [u8], usize>,
+) -> usize {
     if design.len() == 0 {
-        return true;
+        return 1;
     }
 
-    if let Some(&is_possible) = memo.get(design) {
-        return is_possible;
+    if let Some(&num_possibilities) = memo.get(design) {
+        return num_possibilities;
     }
 
-    patterns.iter().filter(|p| design.starts_with(p)).any(|p| {
-        let is_possible = is_possible(patterns, &design[p.len()..], memo);
-        memo.insert(design, is_possible);
-        is_possible
-    })
+    patterns
+        .iter()
+        .filter(|p| design.starts_with(p))
+        .map(|p| {
+            let design = &design[p.len()..];
+            let num_possibilities = num_possibilities(patterns, design, memo);
+            memo.insert(design, num_possibilities);
+            num_possibilities
+        })
+        .sum()
 }
